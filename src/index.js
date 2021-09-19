7bb7cfc3ffe9d5041fa94c97252c574a8658bbec
Daniel Kadã <dgames201615@outlook.com>
2021-09-19 10:18:15 -0300

    configuring eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ app.get('/', (request, response) => {
   response.send('Hello World');
 });
 
-app.listen(3000, () => { 
-  console.log('🔥 Server started at http://localhost:3000')
+app.listen(3000, () => {
+  console.log('🔥 Server started at http://localhost:3000');
 });
