5d884e7995427e54eb2675a7a269263e0f1a0f48
Daniel Kadã <dgames201615@outlook.com>
2021-09-20 04:58:44 -0300

    contact editing route

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -41,7 +41,9 @@ class ContactsRepository {
     ));
   }
 
-  create(name, email, phone, category_id) {
+  create(
+    name, email, phone, category_id,
+  ) {
     return new Promise((resolve) => {
       const newContact = {
         id: v4(),
@@ -52,6 +54,24 @@ class ContactsRepository {
       };
       contacts.push(newContact);
       resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) => (contact.id === id ? updatedContact : contact));
+
+      resolve(updatedContact);
     });
   }
 }
